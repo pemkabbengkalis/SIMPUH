ab3c6a35cdfc9b3d873ebdff894c331de1128ac1
Bohati Mulyadi <bohatimulyadi99@gmail.com>
2023-11-04 15:19:53 +0700

    update css login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,11 +9,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('backend_template') }}/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="/backend_template/plugins/fontawesome-free/css/all.min.css">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset('backend_template') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="/backend_template/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('backend_template') }}/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="/backend_template/dist/css/adminlte.min.css">
   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body class="hold-transition login-page">
@@ -69,10 +69,10 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="{{ asset('backend_template') }}/plugins/jquery/jquery.min.js"></script>
+<script src="/backend_template/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('backend_template') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/backend_template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('backend_template') }}/dist/js/adminlte.min.js"></script>
+<script src="/backend_template/dist/js/adminlte.min.js"></script>
 </body>
 </html>
