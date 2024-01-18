c3f05c294a6eccf91c5e0a4d03e7fb68415fb8c8
Bohati Mulyadi <bohatimulyadi99@gmail.com>
2024-01-19 00:48:44 +0700

    update bug delete sub kegiatan

--- resources/views/back/admin/subkegiatan/index.blade.php
+++ resources/views/back/admin/subkegiatan/index.blade.php
@@ -43,7 +43,7 @@
                       <td>
                           <center>
                             <a href="{{url(modul('path').'/edit/'.en($row->id_sub_kegiatan))}}" class="btn btn-warning btn-xs text-white"><i class="fas fa-edit"></i></a>
-                            <a href="{{url(modul('path').'/delete/'.en($row->id_sub_kegiatan))}}" class="btn btn-danger btn-xs"><i class="fa fa-trash" aria-hidden="true" onclick="return confirm('Yakin akan menghapus data ini?')"></i></a>
+                            <a href="{{url(modul('path').'/delete/'.en($row->id_sub_kegiatan))}}" class="btn btn-danger btn-xs" onclick="return confirm('Yakin akan menghapus data ini?')"><i class="fa fa-trash" aria-hidden="true" ></i></a>
                           </center>
                       </td>
                     </tr>
