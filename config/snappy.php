fb399a533bb1b91302eef3b2b0c4fc2d162131ce
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2024-04-04 09:52:05 +0700

    'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
